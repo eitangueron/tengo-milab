a4a4eef5702be27b6e7593b24513fa26fa2b7ec5
Eitan Gueron <eitangueron@gmail.com>
2022-04-23 20:43:39 +0300

    modal to card

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -41,7 +41,7 @@ function RootNavigator() {
     <Stack.Navigator>
       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
+      <Stack.Group screenOptions={{ presentation: 'card' }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
     </Stack.Navigator>
