fe64b5c8ab6a0849826983a118b9278639dd92a4
Eitan Gueron <eitangueron@gmail.com>
2022-04-30 13:46:13 +0300

    new navigation settings closer to figma

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -7,6 +7,7 @@ import { FontAwesome } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Text } from '@rneui/base';
 import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
 
@@ -37,10 +38,35 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
+  const colorScheme = useColorScheme();
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
+      {/* <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} /> */}
+      <Stack.Screen name="AnswerScreen" component={AnswerScreen} options={({ navigation }: RootTabScreenProps<'AnswerScreen'>) => ({
+          title: '',
+          tabBarActiveTintColor: Colors[colorScheme].tint,
+          tabBarIcon: ({ color }) => <TabBarIcon name="plus" color={color} />,
+          headerRight: () => (
+            <Pressable
+              onPress={() => navigation.navigate('Modal')}
+              style={({ pressed }) => ({
+                opacity: pressed ? 0.5 : 1,
+              })}>
+              <FontAwesome
+                name="user-circle-o"
+                size={25}
+                // color={Colors[colorScheme].text}
+                style={{ marginRight: 15 }}
+              />
+            </Pressable>
+          ),
+          headerLeft: () => (
+            <Text>Tengo .</Text>
+          )
+        })}/>
+      <Stack.Screen name="AddScreen" component={AddScreen} options={{title:""}}/>
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+
       <Stack.Group screenOptions={{ presentation: 'card' }}>
         <Stack.Screen name="Modal" component={ModalScreen} />
       </Stack.Group>
